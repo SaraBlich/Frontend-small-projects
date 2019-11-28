45c45aa38d8a12a24c2a9982e2907d5f58a16146
Sara Blichowska <51542737+SaraBlich@users.noreply.github.com>
2019-11-28 18:23:50 +0100

    stickynav

---
+++ movable_navigation.js
@@ -0,0 +1,31 @@
+$(document).ready(function()
+{
+  //read the top value of nav div
+  var NavY = $(".nav").offset().top;
+
+  var stickyNav = function()
+  {
+    //read the top of actual scroll movement value
+    var ScrollY = $(window).scrollTop();
+
+    if (ScrollY > NavY)
+    {
+      $(".nav").addClass("sticky");
+    }
+    else
+    {
+      $(".nav").removeClass("sticky");
+    }
+  };
+  stickyNav(); //function call
+
+  $(window).scroll(function()
+{
+  stickyNav(); // function call during scrolling
+});
+});
+
+
+
+
+)
